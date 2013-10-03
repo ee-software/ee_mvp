5c847d1cd0ea5326f948fd9addfbf4feb8a5f472
clemmece <clemmece@gmail.com>
2013-10-03 13:47:39 -0400

    new rails project and inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EeMvp::Application.config.secret_key_base = '866a3bc34941daa2a8b638a5c19441a7fe6b43999ae6ff01fa4af0ccba90d7cb87e70068144a8c26b09c2304f867c15f1b6774abb0a591a3f11ece9410a5d44b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EeMvp::Application.config.session_store :cookie_store, key: '_ee_mvp_session'
