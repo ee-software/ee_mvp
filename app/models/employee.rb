42b2a0cadecdc9ac5feab10ea6f3f6717f991aec
clemmece <clemmece@gmail.com>
2013-10-04 13:01:37 -0400

    added some unit tests

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,10 +1,10 @@
 class Employee < ActiveRecord::Base
   has_many :service_calls
-  validates :name, :presence :true
-  validates :address1, :presence :true
-  validates :city, :presence :true
-  validates :state, :presence :true
-  validates :zip, :presence :true
-  validates :start_date, :presence :true
-  validates :job_function, :presence :true
+  validates :name, presence: true
+  validates :address1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :start_date, presence: true
+  validates :job_function, presence: true
 end
